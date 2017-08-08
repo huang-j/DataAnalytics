14c4df33b39b3a762a3e73cd5fb0daa25c1f56fd
huang-j <huang.j@wustl.edu>
2017-08-08 16:53:01 -0500

    added recistPredict function

--- App/tlv3.js
+++ App/tlv3.js
@@ -3,7 +3,9 @@ $(function() {
       therapies = [],
       progression = [],
       recist = [];
-
+function add(a. b){
+  return a + b;
+};
 function parseDate(date, mdy) {
     var tempdate = date;
 console.log(tempdate);
@@ -26,220 +28,81 @@ console.log(tempdate);
   };
 
 
-// // Outdated
-// function followUps(blooddraws, therapies){
-//     var i = 0,
-//         j = 0,
-//         k = 1,
-//         r = 0,
-//         bool = true,
-//         b = 0,
-//         thmoves = 0;
-
-//     var bdkeys = Object.keys(blooddraws),
-//         thkeys = Object.keys(therapies),
-//         rskeys = Object.keys(recist);
-//     while(bool){
-//       if(b >= bdkeys.length){ bool = false }
-//       else if(blooddraws[bdkeys[b]].drawDate <= therapies[thkeys[0]].start){
-//           blooddraws[bdkeys[b]].line = 'Baseline';
-//           b++;
-//       } else { bool = false; };
-//     };
-
-//     while( i < bdkeys.length - 1 && j < thkeys.length && k < bdkeys.length){
-//         if(k <= i){k = i + 1};
-//         var bd = blooddraws[bdkeys[i]],
-//             bd2 = blooddraws[bdkeys[k]],
-//             rs = recist[rskeys[r]],
-//             th = therapies[thkeys[j]];
-        
-//       console.log(bd);
-//       console.log(bd2);
-//       console.log(th);
-//       console.log('thmoves: ' + thmoves);
-//         if(bd['line']){
-//             if(bd2['line'] == 'Baseline'){ i++; k++; continue; }; 
-//                 console.log('bd has line');
-//             if(bd2['drawDate'] < th['end']){
-//                 console.log('bd before th end');
-//                 console.log(thmoves);
-//                 if(bd2['drawDate'] > th['start'] && thmoves == 0){
-//                 console.log('pseudofollowup');
-//                     bd2['pseudoFollowup'] = true;
-//                     k++;
-//                 } else if( bd2['drawDate'] <= th['start'] && thmoves == 1){
-//                         console.log('checking if followup');
-//                     if(therapies[thkeys[j-1]]['line'] != th['line']){
-//                           console.log('followUp');
-//                         bd2['followUp'] = true;
-//                         if(bd2['line']){ } else {
-//                             console.log('setting line A ' + therapies[thkeys[j-1]]['line']);
-//                             bd2['line'] = therapies[thkeys[j-1]]['line'];
-//                         };
-//                         i = k;
-//                         k++;
-//                         thmoves = 0;
-//                     } else {
-//                         bd2['pseudoFollowup'] = true;
-//                         k++;
-//                         thmoves = 0;
-//                     };
-//                 } else if ( thmoves > 0 && th['line'] == therapies[thkeys[j-1]]['line']){
-//                     console.log('testing');
-//                     thmoves = 0;
-//                 } else {
-//                         console.log('should not be here'); 
-//                     break;
-//                 };
-//             } else if (bd2['drawDate'] >= th['end']){
-//                 console.log('bd after th end');
-//                 if(j + 1 == thkeys['length'] && thmoves == 0){
-//                     bd2['followUp'] = true;
-//                     bd2['line'] = th['line'];
-//                     j++;
-//                 } else {
-//                     if(therapies[thkeys[j+1]]){
-//                       if(bd2['drawDate'] <= therapies[thkeys[j+1]]['start']){
-//                           console.log('setting line B: ' + th['line']);
-//                           bd2['line'] = th['line'];
-//                       };
-//                     };
-//                     if(thmoves == 0) {
-//                         j++;
-//                         thmoves++;
-//                     } else {
-//   console.log('bd2 is __line');
-//                         i = k;
-//                         k++;
-//   j++;
-//                         thmoves = 0;
-//                     };
-//                 };
-//             };
-//         } else {
-//             if(bd['drawDate'] > th['start']){
-//                 if(bd['drawDate'] < th['end']){
-//                     i++;
-//                 } else {
-//                     j++;
-//                     thmoves++;
-//                 };
-//             } else if (bd['drawDate'] <= th['start']){
-//                     if(thmoves >= 1){
-//                         console.log('setting log C');
-//                         bd['line'] = therapies[thkeys[j-1]]['line'];
-//                         thmoves = 0;
-//                     } else {
-//                         i++;
-//                     };
-//             };
-//         };
-//     };
-
-// };
-
-// function createDateRanges(therapies, recist){
-//     var thkeys = Object.keys(therapies),
-//         rskeys = Object.keys(recist),
-//         r = 0,
-//         regimen = 0,
-//         pline = '',
-//         bool = false,
-//         firstCT = recist[rskeys[0]],
-//         ranges = [];
-
-//     if (rskeys.length < 2) {
-//         console.log('not enough CT scans');
-//       return
-//     };
-//     for(var i = 0; i < thkeys.length; i++){
-//         var th = therapies[thkeys[i]],
-//             s = 0;
-
-//         if(pline != th.line || pline == ''){
-//             pline = th.line;
-//             regimen += 1;
-//         };
-//         while(bool == false && r < rskeys.length){
-//             var rsImage = recist[rskeys[r]];
-//             if(rsImage['imageDate'] < th['start']){
-//                 r += 1;
-//             } else {
-//                 if(rsImage['imageDate'] <= th['end']){
-//                     s += 1;
-//                     r += 1;
-//                     ranges.push({
-//                         R: regimen,
-//                         S: s,
-//                         start: th['start'],
-//                         end: rsImage['imageDate']
-//                     });
-//                 } else {
-//                     if(thkeys[i+1]){
-//                         if(therapies[thkeys[i+1]]['end'] <= rsImage['imageDate']){
-//                             bool = true;
-//                         } else if(therapies[thkeys[i+1]]['start'] >= rsImage['imageDate']) {
-//                             s += 1;
-//                             r += 1;
-//                             ranges.push({
-//                                 R: regimen,
-//                                 S: s,
-//                                 start: th['start'],
-//                                 end: rsImage['imageDate']
-//                             });
-//                             bool = true;
-//                         } else if(Math.abs(therapies[thkeys[i+1]]['start'] - rsImage['imageDate']) < 604800000 && s == 0){
-//                             s += 1;
-//                             ranges.push({
-//                                 R: regimen,
-//                                 S: s,
-//                                 start: th['start'],
-//                                 end: rsImage['imageDate']
-//                             });
-//                             bool = true;
-//                         } else { bool = true};
-//                     } else {
-//                         s += 1;
-//                         ranges.push({
-//                             R: regimen,
-//                             S: s,
-//                             start: th['start'],
-//                             end: rsImage['imageDate']
-//                         });
-//                         bool = true;
-//                     };
-//                 };
-//             };
-//         };
-//         bool = false;
-//     };
-//     return ranges;
-// };
-
-// function assignRS(ranges, blooddraws){
-//     var ranges = ranges,
-//         blooddraws = blooddraws,
-//         looper = 0,
-//         bdkeys = Object.keys(blooddraws);
-
-//     for(var i = 0; i < bdkeys.length; i++){
-//         var bddd = blooddraws[bdkeys[i]]['drawDate'];
-//         for(var j = 0; j < ranges.length; j++){
-//             var range = ranges[j];
-//             if(bddd >= range.start && bddd <= range.end){
-//                 if(blooddraws[bdkeys[i]]['R']){
-//                     blooddraws[bdkeys[i]]['R'].push(range.R);
-//                     blooddraws[bdkeys[i]]['S'].push(range.S);
-//                 } else {
-//                     blooddraws[bdkeys[i]]['R'] = [range.R];
-//                     blooddraws[bdkeys[i]]['S'] = [range.S];          
-//                 };
-//             }
-//         }; 
-//     };
-//     return blooddraws;
-// };
+  function recistPredict(recist, therapies, blooddraws, weeks){
+    var tindex = 0,
+        bdindex = 0,
+        rsindex = 0,
+        weeks = weeks * 604800000,
+        ep = [],
+        cp = [];
+    while(rsindex < recist.length()){
+      if(recist[rsindex]['imageDate'] < therapies[tindex].start){
+        rsindex += 1;
+      } else if(therapies[tindex + 1]) {
+          if(recist[rsindex]['imageDate'] >= therapies[tindex + 1].start){
+            tindex += 1;
+          };
+      } else {
+          var exopredict = [],
+              cfpredict = [];
+          for(var bd = bdindex; bd < blooddraws.length(); bd++){
+            if(blooddraws[bd]['drawDate'] >= recist[rsindex]['imageDate']){
+              break
+            } else if(blooddraws[bd]['drawDate'] <= recist[rsindex]['imageDate'] - weeks || blooddraws[bd]['drawDate'] < therapies[tindex].start){
+              bdindex += 1;
+            } else {
+              if(recist[rsindex]['event'] = 'PD'){
+                if(blooddraws[bd]['pos'] = 'both'){
+                  exopredict.push(1);
+                  cfpredict.push(1);
+                } else if(blooddraws[bd]['pos'] = 'exoDNA'){
+                  exopredict.push(1);
+                  cfpredict.push(0);
+                } else if(blooddraws[bd]['pos'] = 'cfDNA'){
+                  exopredict.push(0);
+                  cfpredict.push(1);
+                } else {
+                  exopredict.push(0);
+                  cfpredict.push(0);
+                };
+              } else {
+                if(blooddraws[bd]['pos'] = 'both'){
+                  exopredict.push(0);
+                  cfpredict.push(0);
+                } else if(blooddraws[bd]['pos'] = 'exoDNA'){
+                  exopredict.push(0);
+                  cfpredict.push(1);
+                } else if(blooddraws[bd]['pos'] = 'cfDNA'){
+                  exopredict.push(1);
+                  cfpredict.push(0);
+                } else {
+                  exopredict.push(1);
+                  cfpredict.push(1);
+                };;
+              };
+            };
+          };
+          if(exopredict != []){
+            var exofract = exopredict.reduce(add, 0)/exopredict.length();
+          } else {
+            console.log('no blooddraws before restaging');
+            var exofract = 'N/A'
+          };
+          if(exopredict != []){
+            var cffract = cfpredict.reduce(add, 0)/cfpredict.length();
+          } else {
+            console.log('no blooddraws before restaging');
+            var cffract = 'N/A'
+          };
+          recist[rsindex]['exoPrediction'] = exofract;
+          ep.push(exofract);
+          recist[rsindex]['cfPrediction'] = cffract;
+          cp.push(cffract);
+          rsindex += 1;
+      };
+    };
+    return [ep, cp];
+  };
 
   var therapylist = String("& GraphAnalysis::TherapyList &").split(';');
 console.log('therapy list');
@@ -295,25 +158,25 @@ console.log(ddpcr);
         if(ddpcr[j][2] == 'exoDNA'){
           if(color == '#e4e2dc'){ 
             color = '#18e707';
-            point['surge'] = 'exoDNA';
-            blooddraws[i]['surge'] = 'exoDNA';
+            point['pos'] = 'exoDNA';
+            blooddraws[i]['pos'] = 'exoDNA';
             point['exoDNA'] = ddpcr[j][1];
           } else { 
             color = '#4ee4d3';
-            point['surge'] = 'both';
-            blooddraws[i]['surge'] = 'both';
+            point['pos'] = 'both';
+            blooddraws[i]['pos'] = 'both';
             point['exoDNA'] = ddpcr[j][1];
           };
         } else if(ddpcr[j][2] == 'cfDNA'){
           if(color == '#e4e2dc'){
             color = 'Blue';
-            point['surge'] = 'cfDNA';
-            blooddraws[i]['surge'] = 'cfDNA';
+            point['pos'] = 'cfDNA';
+            blooddraws[i]['pos'] = 'cfDNA';
             point['cfDNA'] = ddpcr[j][1];
           } else { 
             color = '#4ee4d3';
-            point['surge'] = 'both';
-            blooddraws[i]['surge'] = 'both';
+            point['pos'] = 'both';
+            blooddraws[i]['pos'] = 'both';
             point['cfDNA'] = ddpcr[j][1];
           };
         };
@@ -337,11 +200,14 @@ console.log(il);
 console.log('recist');
 console.log(recist);
   for(var i = 0; i < Object.keys(recist).length; i++){
-    var image = recist[Object.keys(recist)[i]];
+    var image = recist[i];
     var point = {
        x: 1,
        y: image['imageDate'],
-       event: image['event']
+       event: image['event'],
+    };
+    if(point.event = 'PD'){
+      point.color = 'Red';
     };
     recistG.push(point);
   };
@@ -393,12 +259,12 @@ Highcharts.chart('graph', {
               return '<h3><b>' + this.point.therapyType + '</b></h3><br><b>' + this.point.line + '</b><br><b>' +this.point.regimen + '</b><br>Start Date: ' + Highcharts.dateFormat('%m/%d/%y', this.y) + '<br>End Date: ' + Highcharts.dateFormat('%m/%d/%y', this.point.high);
             } else { return '<h3><b>' + this.point.therapyType + '</b></h3><br><b>' + this.point.regimen + '</b><br>Start Date: ' + Highcharts.dateFormat('%m/%d/%y', this.y) + '<br>End Date: ' + Highcharts.dateFormat('%m/%d/%y', this.point.high);
             }
-          } else if(this.point.surge){
-            if(this.point.surge == 'both'){
+          } else if(this.point.pos){
+            if(this.point.pos == 'both'){
               return '<h3><b>' + this.series.name + '</b></h3><br>' + 'Date: ' + Highcharts.dateFormat('%m/%d/%y', this.y) + '<br>exoDNA: ' + this.point.exoDNA + '<br>cfDNA: ' + this.point.cfDNA;
-            } else if(this.point.surge == 'exoDNA'){
+            } else if(this.point.pos == 'exoDNA'){
               return '<h3><b>' + this.series.name + '</b></h3><br>' + 'Date: ' + Highcharts.dateFormat('%m/%d/%y', this.y) + '<br>exoDNA: ' + this.point.exoDNA;
-            } else if(this.point.surge == 'cfDNA'){
+            } else if(this.point.pos == 'cfDNA'){
 return '<h3><b>' + this.series.name + '</b></h3><br>' + 'Date: ' + Highcharts.dateFormat('%m/%d/%y', this.y) + '<br>cfDNA: ' + this.point.cfDNA;
             };
           } else {
@@ -416,16 +282,6 @@ console.log('blood draws');
 console.log(blooddraws);
 console.log('therapies');
 console.log(therapies);
-console.log('progression');
-console.log(progression);
-
-var ranges = createDateRanges(therapies, recist);
-
-console.log('ranges');
-console.log(ranges);
-var bdRS = assignRS(ranges, blooddraws);
-console.log('blooddraws with RS');
-console.log(bdRS);
 
 // $('#abd').click(function(){
 //    var Lines = '';
