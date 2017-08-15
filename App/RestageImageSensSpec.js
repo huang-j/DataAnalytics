a90fa0526a6e8a592d752bcd983393618dea432b
huang-j <huang.j@wustl.edu>
2017-08-15 14:02:14 -0500

    mostly complete version of this

--- App/RestageImageSensSpec.js
+++ App/RestageImageSensSpec.js
@@ -12,28 +12,29 @@ class SensSpec{
 		this.trueNegatives = trueNegatives;
 		this.falseNegatives = falseNegatives;
 		this.falsePositives = falsePositives;
-	}
+	};
     truePos(){
     	return this.truePositives;
-    }
+    };
     trueNeg(){
     	return this.trueNegatives;
-    }
+    };
     falsePos(){
     	return this.falsePositives;
-    }
+    };
     falseNeg(){
     	return this.falseNegatives;
-    }
+    };
     toMatrix(){
-    	return [[this.truePositives, this.falsePositives, this.truePositives + this.falseNegatives],
-    			[this.falseNegatives, this.trueNegatives, this.truePositives + this.falseNegatives],
+    	return [[this.truePositives, this.falsePositives, this.truePositives + this.falsePositives],
+    			[this.falseNegatives, this.trueNegatives, this.trueNegatives + this.falseNegatives],
     			[this.truePositives + this.falseNegatives, this.falsePositives + this.trueNegatives, 
-    				this.truePositives + this.falseNegatives + this.truePositives + this.falseNegatives]];
-    }
+    				this.truePositives + this.falseNegatives + this.falsePositives + this.trueNegatives]];
+    };
 };
 
 function filterData(data, source, Line, Regimen, Threshold){
+  console.log('filtering data, inputs: ' + source + ', '+ Line + ', ' + Regimen +', ' + Threshold);
 	var data = data,
 		truePositives = 0,
 		trueNegatives = 0,
@@ -59,18 +60,18 @@ function filterData(data, source, Line, Regimen, Threshold){
 		var dna = 2;
 	} else if (source.indexOf('cf') != -1){
 		var dna = 3;
-	}''
+	};
 	for(var i = 0; i < data.length; i++){
 		var temp = data[i].split(',');
 		if(temp[4].indexOf(Line) != -1){
 			if(temp[5].indexOf(Regimen) != -1){
-				if( 'PD' == temp[1] ){
+				if( 'PD' == temp[1] & temp[dna] != '' ){
 					if(temp[dna] > Threshold){
 						truePositives += 1;
 					} else {
 						falseNegatives += 1;
 					};
-				} else if(temp[DNA] <= Threshold){
+				} else if(temp[dna] != ''){
 					if(temp[dna] > Threshold){
 						trueNegatives += 1;
 					} else {
@@ -91,7 +92,7 @@ function addtoTable(tableID, matrix){
 		var row = trows[r];
 		for(var c = 0; c < 3; c++){
 			var newCell = row.insertCell();
-			newCell.appendChild(createTextNode(matrix[r][c]));
+			newCell.appendChild(document.createTextNode(matrix[r-1][c]));
 		}; 
 	};
 };
@@ -102,33 +103,50 @@ function changeTable(tableID, matrix){
 	for(var r = 1; r < trows.length; r++){
 		var row = trows[r];
 		for(var c = 1; c < row.cells.length; c++){
-			row.cells[c].innerHTML = matrix[r][c-1];
+			row.cells[c].innerHTML = matrix[r-1][c-1];
 		}; 
 	};
 };
 
 function newCriteria(){
-	var line = document.getElementById('inputline').innerHTML,
-		regimen = document.getElementById('inputregimen').innerHTML,
-		threshold = document.getElementById('inputthreshold').innerHTML;
+	var line = document.getElementById('inputline').value,
+		regimen = document.getElementById('inputregimen').value,
+		threshold = +document.getElementById('inputthreshold').value;
 	if(threshold == ''){
 		threshold = 0;
 	};
-	return [line, regimen, threshold] 
+	return [line, regimen, threshold];
 };
 
-recistData = String("").split(';');
+function statMeasurements(tableID, sensspec){
+    document.getElementById(tableID+'sens').innerHTML = sensspec.truePos()/(sensspec.truePos() + sensspec.falseNeg());
+    document.getElementById(tableID+'spec').innerHTML = sensspec.trueNeg()/(sensspec.trueNeg() + sensspec.falsePos());
+    document.getElementById(tableID+'prec').innerHTML = sensspec.truePos()/(sensspec.truePos() + sensspec.falsePos());
+    document.getElementById(tableID+'npv').innerHTML = sensspec.trueNeg()/(sensspec.trueNeg() + sensspec.falseNeg());
+   
+};
+
+var recistData = String(\""& RestagingAnalysis::numRecist &"\").split(';');
 
 var exoDNA = filterData(recistData, 'exoDNA', '', '', 0); 
 var cfDNA = filterData(recistData, 'cfDNA', '', '', 0);
-
+console.log(exoDNA);
+console.log(cfDNA);
+console.log(exoDNA.toMatrix());
+console.log(cfDNA.toMatrix());
 addtoTable('exo', exoDNA.toMatrix());
 addtoTable('cf', cfDNA.toMatrix());
+statMeasurements('exo', exoDNA);
+statMeasurements('cf', cfDNA);
+
 
 $('#go').click(function(){
-	var [line, regimen, threshold] = newCriteria;
-	exoDNA = filterData(recistData, 'exoDNA', line, regimen, threshold);
-	cfDNA = filterData(recistData, 'cfDNA', line, regimen, threshold);
+    console.log('go clicked');
+	var criteria = newCriteria();
+	var exoDNA = filterData(recistData, 'exoDNA', criteria[0], criteria[1], criteria[2]);
+	var cfDNA = filterData(recistData, 'cfDNA', criteria[0], criteria[1], criteria[2]);
+    console.log(exoDNA);
+    console.log(cfDNA);
 	changeTable('exo', exoDNA.toMatrix());
 	changeTable('cf', cfDNA.toMatrix());
 });
